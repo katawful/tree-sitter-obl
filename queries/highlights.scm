fff52a2c2059d4d5e80e4335c38724bb0f5fe0ac
katawful <katisntgood@gmail.com>
2022-11-02 15:46:03 -0400

    [queries/highlights] add basic highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,22 @@
+(comment) @comment
+
+[
+  "end"
+  "begin"
+  "scn"
+  "scriptname"
+  "if"
+  "else"
+  "elseif"
+  "while"
+  "loop"
+  "break"
+  "continue"
+  "foreach"
+  "set"
+  "let"
+  "call"
+] @keyword
+
+(function) @function
+user_defined_function: (reference) @function
